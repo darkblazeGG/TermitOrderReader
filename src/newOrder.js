e525b4ad480dd98987302a05c5a1e0d96b62d053
Darkblaze <darkblaze.ru@yandex.ru>
2024-01-30 17:49:20 +0700

    .

--- src/newOrder.js
+++ src/newOrder.js
@@ -546,6 +546,7 @@ function newOrder(file) {
             stages[i].days = days.join(',')
             if (stages[i].stage === 'Распил') {
                 stages[i].days = stages[i].days.split(',').slice(-1).join(',')
+                stages[i].shiftable = false
                 stages[i].term = 1
             }
 
